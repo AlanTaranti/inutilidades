e55ef257ace5033e37e1175e56565483632a0085
Alan Taranti <contato@alantaranti.me>
2026-02-08 19:41:04 -0400

    Add initial blog content including Asian cuisine, Pyrenees, James Webb, meditation, animals, and cryptocurrencies posts.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,38 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+
+const color = computed(() => colorMode.value === 'dark' ? '#020618' : 'white')
+
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: color }
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' }
+  ],
+  htmlAttrs: {
+    lang: 'pt_BR'
+  }
+})
+
+useSeoMeta({
+  titleTemplate: '%s | Blog Inutilidades',
+  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/saas-light.png',
+  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/saas-light.png',
+  twitterCard: 'summary_large_image'
+})
+
+
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <NuxtLoadingIndicator />
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
